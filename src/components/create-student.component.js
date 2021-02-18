b9a66961256e2be1c933e84e08f39529d37c7461
Raj Kumar <interplaytheheads@gmail.com>
2021-02-19 01:53:04 +0200

    Form created for the student input

--- src/components/create-student.component.js
+++ src/components/create-student.component.js
@@ -1,11 +1,33 @@
 import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 export default class CreateStudent extends Component{
     render() {
         return(
-            <div>
-                <p>React Create Student Component!</p>
+            <div class="form-wrapper">
+                <Form>
+                    <Form.Group controlId="Name">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="text"/>
+                    </Form.Group>
+                    
+                    <Form.Group controlId="email">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email"/>
+                    </Form.Group>
+
+                    <Form.Group controlId="RollNo">
+                        <Form.Label>Roll No</Form.Label>
+                        <Form.Control type="text"/>
+                    </Form.Group>
+                
+
+                <Button variant="danger" size="lg" block="block" type="submit">
+                    Create Student
+                </Button>
+                </Form>
             </div>
-        );
+            );
     }
 }
