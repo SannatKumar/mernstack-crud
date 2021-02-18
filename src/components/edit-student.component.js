c7cd7cc51a7dd1627423012faab76f8a13af25d5
Raj Kumar <interplaytheheads@gmail.com>
2021-02-19 01:04:25 +0200

    Simple component template defined for editing student

---
+++ src/components/edit-student.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+export default class EditStudent extends Component{
+    render() {
+        return(
+            <div>
+                <p>React Edit Student Component!</p>
+            </div>
+        );
+    }
+}
