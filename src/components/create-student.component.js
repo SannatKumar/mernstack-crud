dba937e122fc6dcc7dd18df097eaef2a3aa6cf1f
Raj Kumar <interplaytheheads@gmail.com>
2021-02-19 02:07:15 +0200

    Setting up functions, state and, onsubmit defined and included in the form

--- src/components/create-student.component.js
+++ src/components/create-student.component.js
@@ -3,23 +3,65 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 export default class CreateStudent extends Component{
+
+    constructor(props) {
+        super(props)
+
+        //Setting up Functions
+        this.onChangeStudentName = this.onChangeStudentName.bind(this);
+        this.onChangeStudentEmail = this.onChangeStudentEmail.bind(this);
+        this.onChangeStudentRollNo = this.onChangeStudentRollNo.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+
+        //Setting up state
+        this.state = {
+            name: '',
+            email: '',
+            rollno:'' 
+        }
+    }
+
+    onChangeStudentName(e) {
+        this.setState({name: e.target.value})
+    }
+
+    onChangeStudentEmail(e) {
+        this.setState({email: e.target.value})
+    }
+
+    onChangeStudentRollNo(e) {
+        this.setState({rollno: e.target.value})
+    }
+
+    onSubmit(e) {
+        e.preventDefault()
+
+        console.log('Student successfully created!');
+        console.log(`Name: ${this.state.name}`);
+        console.log(`Email: ${this.state.email}`);
+        console.log(`Roll No: ${this.state.rollno}`);
+
+        this.setState({name: '', email: '', rollno:''})
+    }
+
     render() {
         return(
             <div class="form-wrapper">
-                <Form>
+                <Form onSubmit={this.onSubmit}>
                     <Form.Group controlId="Name">
                         <Form.Label>Name</Form.Label>
-                        <Form.Control type="text"/>
+                        <Form.Control type="text" value={this.state.name} onChange={this.onChangeStudentName}/>
                     </Form.Group>
-                    
+
                     <Form.Group controlId="email">
                         <Form.Label>Email</Form.Label>
-                        <Form.Control type="email"/>
+                        <Form.Control type="email" value={this.state.email} onChange={this.onChangeStudentEmail}/>
                     </Form.Group>
 
                     <Form.Group controlId="RollNo">
                         <Form.Label>Roll No</Form.Label>
-                        <Form.Control type="text"/>
+                        <Form.Control type="text" value={this.state.rollno} onChange={this.onChangeStudentRollNo}/>
                     </Form.Group>
                 
 
