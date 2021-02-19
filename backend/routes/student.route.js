b6bf14cefbf236573125ac253913c9db38ea9da8
Raj Kumar <interplaytheheads@gmail.com>
2021-02-19 22:25:57 +0200

    All the functions added

--- backend/routes/student.route.js
+++ backend/routes/student.route.js
@@ -41,3 +41,35 @@ router.route('/edit-student/:id').get((req, res) => {
         }
     })
 });
+
+//Update student
+
+router.route('/update-student/:id').put((req, res, next) => {
+    studentSchema.findByIdAndUpdate(req.params.id, {
+        $set: req.body
+    }, (error, data) => {
+        if (error) {
+            return next(error);
+            console.log(error)
+        }else {
+            res.json(data)
+            console.log('Student updated successfully !')
+        }
+    })
+});
+
+//Delete Student
+
+router.route('/delete-student/:id').delete((req, res, next) => {
+    studentSchema.findByIdAndRemove(req.params.id, (error, data) => {
+        if(error) {
+            return next(error)
+        } else {
+            res.status(200).json({
+                msg: data
+            })
+        }
+    })
+});
+
+module.exports = router;
