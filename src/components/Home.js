33e1b2a1133197495f080c458c532b1705ea74a2
Raj Kumar <interplaytheheads@gmail.com>
2021-02-20 02:10:21 +0200

    Home page Created

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export default class Home extends Component {
+    render() {
+        return(
+            <div>
+                <p>Hi! if Insurance.</p>
+                <p>Compiled successfully!You can now view mernstack-myapp in the browser. 
+                Local:http://localhost:3001        
+                On Your Network:  http://192.168.1.4:3001
+Note that the development build is not optimized.
+To create a production build, use npm run build.</p>
+            </div>
+        );
+    }
+}
