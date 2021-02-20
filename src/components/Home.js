b08b70f142a1e6a2a879044a436a46988c85de5e
Raj Kumar <interplaytheheads@gmail.com>
2021-02-20 17:58:28 +0200

    text changed

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,12 +4,12 @@ export default class Home extends Component {
     render() {
         return(
             <div>
-                <p>Hi! if Insurance.</p>
-                <p>Compiled successfully!You can now view mernstack-myapp in the browser. 
-                Local:http://localhost:3001        
-                On Your Network:  http://192.168.1.4:3001
-Note that the development build is not optimized.
-To create a production build, use npm run build.</p>
+                <p>Hi! There.</p>
+                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
+                    The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making
+                     it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search
+                      for 'lorem ipsum' will uncover many web sites still in their infancy.
+                     Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
             </div>
         );
     }
