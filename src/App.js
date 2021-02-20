9d4955d0bb00f7b2842c1d6d5c93642a8e4b79bc
Raj Kumar <interplaytheheads@gmail.com>
2021-02-20 02:11:07 +0200

    Home link added

--- src/App.js
+++ src/App.js
@@ -1,59 +1,74 @@
 import React from "react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import 'bootstrap/dist/css/bootstrap.css';
-import './App.css';
-
-import {BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import CreateStudent from './components/create-student.component';
-import EditStudent from './components/edit-student.component';
-import StudentList from './components/student-list.component';
+import Home from "./components/Home.js";
+import CreateStudent from "./components/create-student.component.js";
+import EditStudent from "./components/edit-student.component.js";
+import StudentList from "./components/student-list.component.js";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
 
 function App() {
-  return (<Router>
-    <div className = "App">
-      <header className ="App-header">
-        <Navbar bg="dark" variant="dark">
-          <Container>
-            <Navbar.Brand>
-              <Link to={"/create-student"} className = "nav-link">
-                React MERN Stack App
-              </Link>
-            </Navbar.Brand>
-            <Nav className="justify-content-end">
-              <Nav>
-                <Link to={"/create-student"} className="nav-link">
-                  Create Student
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <header>
+          <Navbar bg="dark" variant="dark">
+
+            <Container>
+
+              <Navbar.Brand>
+                <Link to={"/home"} className="nav-link">
+                  React MERN Stack App
                 </Link>
+              </Navbar.Brand>
+              <Nav className="justify-content-end">
+
+                <Nav>
+                  <Link to={"/create-student"} className="nav-link">
+                    Create Student
+                  </Link>
+                </Nav>
+                <Nav>
+                  <Link to={"/edit-student/:id"} className="nav-link">
+                    Edit Student
+                  </Link>
+                </Nav>
+
+                <Nav>
+                  <Link to={"/student-list"} className="nav-link">
+                    Student List
+                  </Link>
+                </Nav>
+
               </Nav>
-              <Nav>
-                <Link to={"/student-list"} className="nav-link">
-                  Student List
-                </Link>
-              </Nav>
-            </Nav>
-          </Container>
-        </Navbar>
-      </header>
-      <Container>
-        <Row>
-          <Col md={12}>
-            <div className="wrapper">
-            <Switch>
-              <Route exact path='/' component={CreateStudent} />
-              <Route exact path="/create-student" component={CreateStudent} />
-              <Route exact path="/edit-student/:id" component={EditStudent} />
-              <Route exact path="/student-list" component={StudentList} />
-            </Switch>
-            </div>
-          </Col>
-        </Row>
-      </Container>
+            </Container>
+
+          </Navbar>
+        </header>
+
+        <Container>
+          <Row>
+            <Col md={12}>
+              <div className="wrapper">
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route path="/create-student" component={CreateStudent} />
+                  <Route path="/edit-student/:id" component={EditStudent} />
+                  <Route path="/student-list" component={StudentList} />                  
+                </Switch>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+
+      </BrowserRouter>
     </div>
-  </Router>);
+  );
 }
 
 export default App;
